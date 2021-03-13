335f5718ff0a76e27f3859605b989d11c4ef2bec
ajay_gidwani <ajay.gidwani@synoriq.in>
2021-03-14 00:41:23 +0530

    Implemented Point two. Handling unknown amount of numbers

--- Driver.java
+++ Driver.java
@@ -1,8 +1,13 @@
 public class Driver{
     public static void main(String[] args) {
         StringCalculator sc = new StringCalculator();
+
+        // first point test case, empty string, one number and two numbers
         System.out.println(sc.add(""));
         System.out.println(sc.add("2"));
         System.out.println(sc.add("2,3"));
+
+        // test cases for point 2, handling unknown amount of numbers
+        System.out.println(sc.add("1,2,3,4,5"));
     }
 }
--- StringCalculator.java
+++ StringCalculator.java
@@ -13,7 +13,22 @@ class StringCalculator {
         // getting numbers
         String num[] = numbers.split(",");
 
+        // handling unknown amount of numbers
+        if(num.length > 2) {
+            int result = handleUnknownAmountOfNumbers(num);
+            return result;
+        } 
+
         // if string has two numbers
         return Integer.valueOf(num[0]) + Integer.valueOf(num[1]); 
+    }
+
+    private int handleUnknownAmountOfNumbers(String num[]) {
+        int sum = 0;
+        for(int i = 0 ; i < num.length ; i++) {
+            int number = Integer.valueOf(num[i]);
+            sum = sum + number;
+        }
+        return sum;
     }
 }
