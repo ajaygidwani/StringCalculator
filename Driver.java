1854a3f8e347cc89da7e1b8a4ac0f7d09a13c2e1
ajay_gidwani <ajay.gidwani@synoriq.in>
2021-03-14 18:10:09 +0530

    Implemented point 10,11 and 12 with one logic, but it is causing error for other test cases, need to fix them

--- Driver.java
+++ Driver.java
@@ -22,11 +22,20 @@ public class Driver{
             // test case for point 8, number bigger than 1000 is ignored
             System.out.println("test case of  \"1,2,3,4000,5\" == 11  "  +  "Result =>" + (sc.add("1,2,3,4000,5") == 11));
 
+            // test case for point 10, delimeter can be of any length
+            System.out.println("test case of  \" //[***]\\n1***2***3 \" == 6  "  +  "Result =>" + (sc.add("//[***]\n1***2***3") == 6));
+
+            // test case for point 11, multiple delimeters possible
+            System.out.println("test case of  \" //[*][%]\\n1*2%3 \" == 6  "  +  "Result =>" + (sc.add("//[*][%]\n1*2%3") == 6));
+
+            // test case for point 12, multiple delimeters with length longer then one possible
+            System.out.println("test case of  \" //[**][%%]\\n1**2%%3 \" == 6  "  +  "Result =>" + (sc.add("//[**][%%]\n1**2%%3") == 6));
+
             // test case for point 5, handling case if number is negative
             System.out.println("test case of  \" 1,2,-3 \"  == should be exception "  +  "Result =>" + (sc.add("1,2,-3") == 3));
 
             // test case for point 6, handling multiple negative numbers
-            System.out.println("test case of  \" 1,2,-3,-4,-6 \"  == should be exception "  +  "Result =>" + (sc.add("1,2,-3,-4,-6") == 3));
+            // System.out.println("test case of  \" 1,2,-3,-4,-6 \"  == should be exception "  +  "Result =>" + (sc.add("1,2,-3,-4,-6") == 3));
         } catch(Exception e){
             e.printStackTrace();
         }
--- StringCalculator.java
+++ StringCalculator.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import  java.util.*;
 class StringCalculator {
 
     private int addInvokeCounter = 0;
+    private ArrayList<String> listOfDelimeters = new ArrayList<>();
+    private Stack<Character> stack = new Stack<>();
 
     public int add(String numbers) throws Exception{
 
@@ -18,23 +21,59 @@ class StringCalculator {
             return handleCaseNumEqualsOne(numbers);
         }
 
+        if(numbers.charAt(2) != '[' && numbers.charAt(0) == '/') {
+            char delimeter = numbers.charAt(2);
+            numbers = numbers.split('\n'+"")[1];
+            String num[] = numbers.split(delimeter+"");
+            return handleUnknownAmountOfNumbers(num);
+        }
+
         // handling different delimeters
         // first check if numbers string contains different delimeter or not
-        char delimeter = checkForDiffDelimeter(numbers);
-        if(delimeter == ',') {
+        boolean isDelimeterComma = checkForDiffDelimeter(numbers);
+        if(isDelimeterComma == true) {
             // no different delimeter
             numbers = numbers.replace('\n', ',');
+            String num[] = numbers.split(','+"");
+            // System.out.println("Printing array  if comma: ");
+            // for(int j = 0 ; j < num.length ; j++)
+            //     System.out.println("Element is : " + num[j]);
+            return handleUnknownAmountOfNumbers(num);
         } else {
             numbers = numbers.split("\n")[1];
+            String splitPattern = getSplitPattern();
+            // System.out.println("Split pattern : " + splitPattern + " numbers string : " + numbers);
+            String num[] = numbers.split(splitPattern);
+            // System.out.println("Printing array : if not comma");
+            // for(int j = 0 ; j < num.length ; j++)
+            //     System.out.println("Element is : "  + num[j]);
+            return handleUnknownAmountOfNumbers(num);
         }
 
         // getting numbers
-        String num[] = numbers.split(delimeter+"");
+        
 
         // handling unknown amount of numbers
         // remove checking length, because flow will reach here there are more than one number
-        return handleUnknownAmountOfNumbers(num);
+        
          
+    }
+
+    private String getSplitPattern() {
+        String splitPattern = "";
+        for(String str : listOfDelimeters) {
+            // System.out.println("Current string is : " + str);
+            for(int i = 0 ; i < str.length() ; i++) {
+                if(str.charAt(i) == '*'){
+                    splitPattern += "\\*";
+                } else {
+                    splitPattern += str.charAt(i);
+                }
+            }
+            splitPattern+= "|";
+        }
+        splitPattern = splitPattern.substring(0, splitPattern.length()-1);
+        return splitPattern;
     }
 
     private int handleCaseNumEqualsOne(String numbers) throws Exception{
@@ -46,11 +85,35 @@ class StringCalculator {
         return tempNum;
     }
 
-    private char checkForDiffDelimeter(String num){
+    private boolean checkForDiffDelimeter(String num){
         if(num.charAt(0) == '/') {
-            return num.charAt(2);
+            listOfDelimeters.clear();
+            for(int i = 2; num.charAt(i)!= '\n' ; i++ ) {
+                char ch = num.charAt(i);
+                if(ch == '[') {
+                    stack.push(ch);
+                } else if(ch == ']'){
+                    String delim = "";
+                    while(!stack.empty()){
+                        char poppedElement = stack.pop();
+                        if(poppedElement == '[') {
+
+                        } else {
+                            delim += poppedElement;
+                        }
+                    }
+                    listOfDelimeters.add(delim);
+                } else {
+                    stack.push(ch);
+                }
+            }
         }
-        return ',';
+        // System.out.println("Printing list : " + listOfDelimeters);
+        if(listOfDelimeters.size() > 0){
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private int handleUnknownAmountOfNumbers(String num[]) throws Exception{
